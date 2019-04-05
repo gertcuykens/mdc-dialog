7b05e349fa308dc3e75c8978f7a3e14004c6bc3f
Gert Cuykens <gert.cuykens@gmail.com>
2019-04-05 13:09:26 +0200

    _

---
+++ index.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# npm i http-server -g
+# npm i typescript -g
+# npm i sass -g
+
+npm i
+tsc
+sass -I node_modules mdc-dialog.scss dist/mdc-dialog.css
+cp -r node_modules dist
+cp -r index.html dist
+cp -r focus-trap.js dist
